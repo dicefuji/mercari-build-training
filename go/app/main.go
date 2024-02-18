be3a1f6a318686015b39f06474d4d12d69e5d374
dicefuji <139557004+dicefuji@users.noreply.github.com>
2024-02-18 20:21:56 +0100

    Update main.go

--- go/app/main.go
+++ go/app/main.go
@@ -27,7 +27,7 @@ func root(c echo.Context) error {
 
 func addItem(c echo.Context) error {
 	// Get form data
-	//name := c.FormValue("name")
+	name := c.FormValue("name")
 	category := c.FormValue("category") // i made this change
 	c.Logger().Infof("Receive item: %s", name, category)
 
