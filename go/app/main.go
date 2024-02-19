2f1f43658a456d3b5b84b03e34f579d32b3a0140
dicefuji <fujiwaraadaisuke@gmail.com>
2024-02-19 22:47:34 +0100

    step 3-3

--- go/app/main.go
+++ go/app/main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"io"
 	"path"
 	"strings"
-
+	
+	"encoding/json"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
@@ -25,12 +27,53 @@ func root(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 }
 
+type Item struct {
+	Name string`json:"name"`
+	Category string `json:"category"`
+}
+
+type ItemsData struct {
+	Items [] Item `json:"items"`
+}
+
+func appendItemToFile(name string, category string) error {
+	file, err := os.OpenFile("items.json", os.O_RDWR|os.O_CREATE, 0755) // Assume file exists
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	itemsData := ItemsData{} // Fill with existing data
+	err = json.NewDecoder(file).Decode(&itemsData)
+	if err != nil && err != io.EOF {
+		return err
+	}
+
+	newItem := Item{Name: name, Category: category} // Adding  new item
+	itemsData.Items = append(itemsData.Items, newItem)
+
+	file.Truncate(0) // Clear file
+	file.Seek(0, 0) // Go to start of file
+	err = json.NewEncoder(file).Encode(itemsData) // Writing in  new data
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 func addItem(c echo.Context) error {
 	// Get form data
 	name := c.FormValue("name")
 	category := c.FormValue("category") // i made this change
 	c.Logger().Infof("Receive item: %s, Category: %s", name, category)
 
+	err := appendItemToFile(name, category) // Append item to file
+	if err != nil {
+		c.Logger().Errorf("Error appending item to file: %s", err)
+		res := Response{Message: "Error appending item to file"}
+		return c.JSON(http.StatusInternalServerError, res)
+	}
+	c.Logger().Infof("Receive item: %s, Category: %s", name, category)
 	message := fmt.Sprintf("item received: %s, category: %s", name,category)
 	res := Response{Message: message}
 
